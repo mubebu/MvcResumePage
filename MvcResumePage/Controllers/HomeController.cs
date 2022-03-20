a3019ab1b5d90df753e1638894c965a9ff937e01
Muhlis B. Budak <budak.berat@gmail.com>
2022-03-20 21:53:20 +0300

    Add project files.

---
+++ MvcResumePage/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcResumePage.Models;
+
+namespace MvcResumePage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ResumeDBEntities db = new ResumeDBEntities();
+            var model = db.UserInfo.Find(3);
+            return View(model);
+        }                
+    }
+}
